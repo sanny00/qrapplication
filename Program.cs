19157a777db195861f5f7e65ae5e3ae2cfd0c34b
sangavi.shanthakumar <sangavi.shanthakumar@tu-dortmund.de>
2021-09-28 23:31:42 +0200

    .gitignore file was created

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace qrapplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi!");
         }
     }
 }
